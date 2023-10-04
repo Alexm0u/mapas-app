16882b7f129865dfb8922038b27f2de344e59a7a
Álex <alejandro.mcamps@bosonit.com>
2023-10-04 14:37:20 +0200

    Mostrar resultados de búsqueda + cambio FEATURE campos no coinciden

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Feature, PlacesResponse } from '../interfaces/places';
-import { PlacesApiClient } from '../api/placesApiClient'; 
+import { PlacesApiClient } from '../api/placesApiClient';
 
 @Injectable({
   providedIn: 'root',
@@ -21,32 +21,37 @@ export class PlacesService {
 
   public async getUserLocation(): Promise<[number, number]> {
     return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(({ coords }) => {
-        this.userLocation = [coords.longitude, coords.latitude];
-        resolve(this.userLocation);
-      },
-      (err) => {
-        alert('No se pudo obtener la geolocalización')
-        reject();
-      });
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          this.userLocation = [coords.longitude, coords.latitude];
+          resolve(this.userLocation);
+        },
+        (err) => {
+          alert('No se pudo obtener la geolocalización');
+          reject();
+        }
+      );
     });
   }
 
-  getPlacesByQuery(query: string ='') {
-
-    if (!this.userLocation) throw Error ('No hay userLocation')
+  getPlacesByQuery(query: string = '') {
+    if(query.length === 0 ) {
+      this.places = [];
+      this.isLoadingPlaces = false;
+      return;
+    }
+    if (!this.userLocation) throw Error('No hay userLocation');
 
     this.isLoadingPlaces = true;
-    this.placesApi.get<PlacesResponse>(`/${query}.json`, {
-      params: {
-        proximity: this.userLocation.join(',')
-      }
-    })
-    .subscribe(resp => {
-      console.log(resp.features)
-
-      this.isLoadingPlaces = false;
-      this.places = resp.features;
-    });
+    this.placesApi
+      .get<PlacesResponse>(`/${query}.json`, {
+        params: {
+          proximity: this.userLocation.join(','),
+        },
+      })
+      .subscribe((resp) => {
+        this.isLoadingPlaces = false;
+        this.places = resp.features;
+      });
   }
 }

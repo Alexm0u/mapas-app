d8544505d4f4c707d614a73b192c3397e752fa62
Álex <alejandro.mcamps@bosonit.com>
2023-10-04 08:07:31 +0200

    Search Bar works

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class PlacesService {
     return !!this.userLocation;
   }
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.getUserLocation();
   }
 
@@ -25,5 +26,10 @@ export class PlacesService {
         reject();
       });
     });
+  }
+
+  getPlacesByQuery(query: string ='') {
+    this.http.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?proximity=-0.3289427168157033%2C39.48322536517395&language=es&access_token=pk.eyJ1IjoicmFxdWVsMjI5MiIsImEiOiJjbG10ODFteWYwMzA0MndxcGpsNW9rd21vIn0.t1wyjbjuYEA-ZHWZs44PIg`)
+    .subscribe(console.log);
   }
 }
